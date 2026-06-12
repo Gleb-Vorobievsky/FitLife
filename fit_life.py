cf006050c789cd94b8ec8f614c37bf1d63c67ee0
Глеб Воробьевский <marz3l@yandex.ru>
2026-06-12 11:34:17 +0300

    Исправленная версия

--- fit_life.py
+++ fit_life.py
@@ -1,6 +1,9 @@
 # Проект FitLife - MVP версия 1.0
 WATER_PER_KG = 30
 MILL_IN_LITER = 1000
+SMALL_AGE = 10
+MIDDLE_AGE = 20
+SMAAL_AGE_LIST = (1, 2, 3, 4)
 
 # Знакомство
 user_name = input('Привет! Напиши свое имя:')
@@ -11,20 +14,26 @@ while True:
     except ValueError:
         print("Напиши возраст целым числом")
 
-# Сбор данных
-while True:
-    try:
-        user_weight = float(input('Напиши свой вес в кг (например: 64):'))
-        break
-    except ValueError:
-        print("Напиши свой вес числом с плавающей точкой, либо без нее")
 
-while True:
-    try:
-        user_height = float(input('Напиши свой рост в м (например: 1.7):'))
-        break
-    except ValueError:
-        print("Напиши свой рост числом с плавающей точкой, либо без нее")
+# Сбор данных
+def input_user_weight():
+    """Функция для корректного ввода веса пользователя"""
+    while True:
+        try:
+            user_weight = float(input('Напиши свой вес в кг (например: 64):'))
+            return user_weight
+        except ValueError:
+            print("Напиши свой вес числом с плавающей точкой, либо без нее")
+
+
+def input_user_height():
+    """Функция для корректного ввода роста пользователя"""
+    while True:
+        try:
+            user_height = float(input('Напиши свой рост в м (например: 1.7):'))
+            return user_height
+        except ValueError:
+            print("Напиши свой рост числом с плавающей точкой, либо без нее")
 
 
 def bmi(user_weight, user_height):
@@ -32,9 +41,8 @@ def bmi(user_weight, user_height):
     """Функция будет возвращать либо целые числа либо дробные"""
     index = round(user_weight / (user_height ** 2), 1)
     if index % 1 == 0:
-        return int(index)
-    else:
-        return index
+        index = int(index)
+    return index
 
 
 def water_normal(user_weight):
@@ -42,30 +50,29 @@ def water_normal(user_weight):
     """Функция будет возвращать либо целые числа либо дробные"""
     user_normal_water = (user_weight * WATER_PER_KG) / MILL_IN_LITER
     if user_normal_water % 1 == 0:
-        return int(user_normal_water)
-    else:
-        return user_normal_water
+        user_normal_water = int(user_normal_water)
+    return user_normal_water
 
 
 def symbol(user_age):
     """Объявим функцию для вычисления"""
     """корректного отображения возраста в 'годах' или 'лет'"""
-    if user_age < 10:
-        if user_age in [1, 2, 3, 4]:
+    if user_age < SMALL_AGE:
+        if user_age in SMAAL_AGE_LIST:
             return "г."
-        else:
-            return "л."
-    elif 10 <= user_age < 20:
         return "л."
-    elif user_age >= 20:
-        if user_age % 10 in [1, 2, 3, 4]:
+    elif SMALL_AGE <= user_age < MIDDLE_AGE:
+        return "л."
+    elif user_age >= MIDDLE_AGE:
+        if user_age % 10 in SMAAL_AGE_LIST:
             return "г."
-        else:
-            return "л."
+        return "л."
 
 
-index_user_weight = bmi(user_weight, user_height)
-water_for_user = water_normal(user_weight)
+total_user_weight = input_user_weight()
+total_user_height = input_user_height()
+index_user_weight = bmi(total_user_weight, total_user_height)
+water_for_user = water_normal(total_user_weight)
 symbol_for_age = symbol(user_age)
 
 # Вывод красивого результата
